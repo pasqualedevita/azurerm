cc72c183866b3689f8ddace501db31aee8986d53
uolter <walter.traspadini@pagopa.it>
2021-06-29 17:47:30 +0200

    first draft module monitor diagnostic settings

---
+++ monitor_diagnostic_setting/README.md
@@ -0,0 +1,28 @@
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_azurerm"></a> [azurerm](#provider\_azurerm) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [azurerm_monitor_diagnostic_setting.this](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_diagnostic_setting) | resource |
+| [azurerm_subscription.subscription](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subscription) | data source |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
---
+++ monitor_diagnostic_setting/main.tf
@@ -0,0 +1,44 @@
+data "azurerm_subscription" "subscription" {
+}
+
+resource "azurerm_monitor_diagnostic_setting" "this" {
+  name                       = var.name
+  target_resource_id         = var.target_resource_id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+  storage_account_id         = var.storage_account_id
+
+  eventhub_name = var.eventhub_name
+  # required due to this issue: https://github.com/terraform-providers/terraform-provider-azurerm/issues/6356
+  eventhub_authorization_rule_id = var.eventhub_authorization_rule != null ? join("/", ["/subscriptions/", data.azurerm_subscription.subscription.subscription_id,
+    "resourceGroups", var.eventhub_resource_group_name, "providers/Microsoft.EventHub/namespaces", var.eventhub_namespace_name,
+    "authorizationrules",
+  var.eventhub_authorization_rule]) : null
+
+  # Note: retention_policies are still required even if it is not useful
+  #       when we use the log analytics workspace.
+  dynamic "log" {
+    iterator = l
+    for_each = var.logs
+    content {
+      category = l.value.category
+      enabled  = l.value.enabled
+      retention_policy {
+        enabled = l.value.retention_policy.enabled
+        days    = l.value.retention_policy.days
+      }
+    }
+  }
+
+  dynamic "metric" {
+    iterator = m
+    for_each = var.metrics
+    content {
+      category = m.value.category
+      enabled  = m.value.enabled
+      retention_policy {
+        enabled = m.value.retention_policy.enabled
+        days    = m.value.retention_policy.days
+      }
+    }
+  }
+}
