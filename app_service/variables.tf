25a99c4330ab3589f3142aced62372635e20f3e8
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-02-13 21:55:05 +0100

    chore: Lint (#22)

--- app_service/variables.tf
+++ app_service/variables.tf
@@ -4,6 +4,9 @@ variable "location" {
   description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
 }
 
+
+
+
 variable "resource_group_name" {
   type        = string
   description = "(Required) The name of the resource group in which to create the App Service and App Service Plan."
